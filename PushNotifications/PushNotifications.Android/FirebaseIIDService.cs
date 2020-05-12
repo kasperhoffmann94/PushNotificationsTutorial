24fc21246214b32cd9b6c74e82a52d5d62de79bf
Kasper <kasperhoffmann@hotmail.dk>
2020-05-12 19:29:53 +0200

    Made connection with firebase

---
+++ PushNotifications/PushNotifications.Android/FirebaseIIDService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Iid;
+
+namespace PushNotifications.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENET" })]
+    public class FirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "FirebaseIIDService";
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            SendRegistrationToServer(refreshedToken);
+        }
+
+        public void SendRegistrationToServer(string token)
+        {
+            // send this token to server
+
+        }
+    }
+}
