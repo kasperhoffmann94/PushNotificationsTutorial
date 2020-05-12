ebe6a69bfc6aa47cdae6bdc48b4deb6647d613fe
Kasper <kasperhoffmann@hotmail.dk>
2020-05-12 13:45:50 +0200

    tried to follow tutorial but is stuck at 3:41

---
+++ PushNotifications/PushNotifications.Android/GSMService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+[assembly: Permission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: UsesPermission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
+[assembly: UsesPermission(Name = "com.google.android.c2dm.permission.RECIEVE")]
+[assembly: UsesPermission(Name = "android.permission.INTERNET")]
+[assembly: UsesPermission(Name = "android.permission.WAKE_LOCK")]
+// get accounts is for android versions 4.0.3 and below
+[assembly: UsesPermission(Name = "android.permission.GET_ACCOUNTS")]
+
+namespace PushNotifications.Droid
+{
+    [BroadcastReceiver(Permission = Gcm.Client.Constants.PERMISSION_GCM_INTENTS)]
+    public class GSMService : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Toast.MakeText(context, "Received intent!", ToastLength.Short).Show();
+        }
+    }
+}
